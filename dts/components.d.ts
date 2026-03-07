5209a763b3e2751865aa5a6e37acc8ba1ea6b328
cnguu <www@cnguu.cn>
2026-03-07 11:15:49 +0800

    feat: 添加 Element Plus 按钮组件并配置 Prettier 忽略规则

--- dts/components.d.ts
+++ dts/components.d.ts
@@ -12,6 +12,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    ElButton: typeof import('element-plus/es')['ElButton']
     HelloWorld: typeof import('./../src/components/HelloWorld.vue')['default']
     IconCommunity: typeof import('./../src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./../src/components/icons/IconDocumentation.vue')['default']
@@ -27,14 +28,15 @@ declare module 'vue' {
 
 // For TSX support
 declare global {
-  const HelloWorld: (typeof import('./../src/components/HelloWorld.vue'))['default']
-  const IconCommunity: (typeof import('./../src/components/icons/IconCommunity.vue'))['default']
-  const IconDocumentation: (typeof import('./../src/components/icons/IconDocumentation.vue'))['default']
-  const IconEcosystem: (typeof import('./../src/components/icons/IconEcosystem.vue'))['default']
-  const IconSupport: (typeof import('./../src/components/icons/IconSupport.vue'))['default']
-  const IconTooling: (typeof import('./../src/components/icons/IconTooling.vue'))['default']
-  const RouterLink: (typeof import('vue-router'))['RouterLink']
-  const RouterView: (typeof import('vue-router'))['RouterView']
-  const TheWelcome: (typeof import('./../src/components/TheWelcome.vue'))['default']
-  const WelcomeItem: (typeof import('./../src/components/WelcomeItem.vue'))['default']
+  const ElButton: typeof import('element-plus/es')['ElButton']
+  const HelloWorld: typeof import('./../src/components/HelloWorld.vue')['default']
+  const IconCommunity: typeof import('./../src/components/icons/IconCommunity.vue')['default']
+  const IconDocumentation: typeof import('./../src/components/icons/IconDocumentation.vue')['default']
+  const IconEcosystem: typeof import('./../src/components/icons/IconEcosystem.vue')['default']
+  const IconSupport: typeof import('./../src/components/icons/IconSupport.vue')['default']
+  const IconTooling: typeof import('./../src/components/icons/IconTooling.vue')['default']
+  const RouterLink: typeof import('vue-router')['RouterLink']
+  const RouterView: typeof import('vue-router')['RouterView']
+  const TheWelcome: typeof import('./../src/components/TheWelcome.vue')['default']
+  const WelcomeItem: typeof import('./../src/components/WelcomeItem.vue')['default']
 }
